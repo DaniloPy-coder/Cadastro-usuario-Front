8083a632831078bc1965b989334d368607152840
DaniloPy-coder <danilojosehapper@gmail.com>
2025-04-16 11:26:28 -0300

    Primeiro commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000'
-})
+    baseURL: process.env.NODE_ENV === 'development'
+        ? 'http://localhost:3000'  // Para desenvolvimento local
+        : process.env.REACT_APP_PUBLIC_API // Para produção
+});
 
-export default api 
+export default api;
